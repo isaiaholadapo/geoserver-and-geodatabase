adc9f4ba9deec6c5d95f5f2e8fd6ef6f63abed0a
Isaiah <daezyworld@gmail.com>
2021-09-20 19:52:58 +0100

    code refactored

--- app.py
+++ app.py
@@ -14,19 +14,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissecret!'
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/geodata'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOADED_FILES'] = UPLOADED_FILES
 
 db = SQLAlchemy(app)
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class AoiCoordinate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     aoi = db.Column(db.String(50), nullable=False)
     coordinate = db.Column(Geometry('POLYGON'))
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -59,37 +53,6 @@ def all_coordinate():
         all_cord.append(location)
 
     return render_template('all.html', all_coordinates=all_cord)
-
-@app.route('/test', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('Please select a file')
-            return redirect(request.url)
-        file = request.files['file']
-
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOADED_FILES'], filename))
-
-            return redirect(url_for('download_file', name=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
-@app.route('/download_file', methods=['GET'])
-def download_file():
-    return 'done'
-
 
 
 if __name__ == "__main__":
